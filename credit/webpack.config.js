2acf2af8ed028f286bb787125951605ddb2bb655
robert88 <841354162@qq.com>
2018-08-22 17:55:20 +0800

    Update webpack.config.js

--- credit/webpack.config.js
+++ credit/webpack.config.js
@@ -1,6 +1,16 @@
+const ConsoleLogOnBuildWebpackPlugin = require("./webpackCoustomPlugin_Test.js")
 exports=module.exports=function(){
 	return {
 		mode:"development",
-		devtool:"source-map"
+		devtool:"source-map",
+		plugins:[
+			new ConsoleLogOnBuildWebpackPlugin()
+
+		],
+		  resolve: {
+		    alias: {
+		      'vue$': 'vue/src/core/index.js' // 'vue/dist/vue.common.js' for webpack 1
+		    }
+		 }
 	}
 }
