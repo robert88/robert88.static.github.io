397b0607cf31fec4b6e7c053ae715f4c472f4864
‘841354162@qq.com’ <841354162@qq.com>
2018-09-02 22:52:20 +0800

    r

--- credit/src/index.js
+++ credit/src/index.js
@@ -1,5 +1,17 @@
 import Vue from 'vue';
+import Alert from "./component/Alert.vue"
 
+
+// 2.注册组件，并指定组件的标签，组件的HTML标签为<my-component>
+Vue.component('matrix-component',{
+	template: '<div style="font-family:微软雅黑;">This is matrix first component</div>'
+});
+
+Vue.component(Alert.name, Alert);
+
+// 3.创建一个 Vue 实例或 "ViewModel"
+// 它连接 View 与 Model
 new Vue({ // eslint-disable-line
 data:{d:1}
 }).$mount('#app');
+
--- credit/webpack.config.js
+++ credit/webpack.config.js
@@ -1,16 +1,30 @@
 const ConsoleLogOnBuildWebpackPlugin = require("./webpackCoustomPlugin_Test.js")
-exports=module.exports=function(){
+exports = module.exports = function () {
 	return {
-		mode:"development",
-		devtool:"source-map",
-		plugins:[
+		mode: "development",
+		devtool: "source-map",
+		// loader: {test: /\.vue$/, loader: "vue-loader"},
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader'
+				},
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/,
+					include: resolve('src')
+				}
+			]
+		},
+		plugins: [
 			new ConsoleLogOnBuildWebpackPlugin()
-
 		],
-		  resolve: {
-		    alias: {
-		      'vue$': 'vue/src/core/index.js' // 'vue/dist/vue.common.js' for webpack 1
-		    }
-		 }
+		resolve: {
+			alias: {
+				'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+			}
+		}
 	}
 }
