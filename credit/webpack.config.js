8accd9e8c8e201c73398e6b675ca03c2e45fc8a4
‘841354162@qq.com’ <841354162@qq.com>
2018-09-10 22:51:06 +0800

    r

--- credit/webpack.config.js
+++ credit/webpack.config.js
@@ -4,6 +4,7 @@ function resolve(file) {
 	console.log(path.resolve(__dirname,file))
 	return path.resolve(__dirname,file);
 }
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 exports = module.exports = function () {
 	return {
 		mode: "development",
@@ -14,17 +15,18 @@ exports = module.exports = function () {
 				{
 					test: /\.vue$/,
 					loader: 'vue-loader'
-				},
-				{
-					test: /\.js$/,
-					loader: 'babel-loader',
-					exclude: /node_modules/,
-					include: resolve('./src')
 				}
+				// {
+				// 	test: /\.js$/,
+				// 	loader: 'babel-loader',
+				// 	exclude: /node_modules/,
+				// 	include: resolve('./src')
+				// }
 			]
 		},
 		plugins: [
-			new ConsoleLogOnBuildWebpackPlugin()
+			new ConsoleLogOnBuildWebpackPlugin(),
+	    new VueLoaderPlugin()
 		],
 		resolve: {
 			alias: {
